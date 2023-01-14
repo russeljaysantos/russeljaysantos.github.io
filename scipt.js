0e372b6dc3622f2c7e513443af3d5d6ab83424a3
russeljaysantos <122032042+russeljaysantos@users.noreply.github.com>
2023-01-14 11:56:48 +0800

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,7 @@
+// typing text animation script
+    var typed = new Typed(".input", {
+                strings:["a BSIT Student.","from BSIT 1B."],
+                typeSpeed: 70,
+                backSpeed: 60,
+                loop:true
+            });
